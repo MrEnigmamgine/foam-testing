63cb87906f345c0f5e323022af1db68d5a56dfdd
MrEnigmamgine <mayes.joshua.d@gmail.com>
2024-03-04 22:32:21 +0000

    Automatic commit Mon Mar  4 22:32:21 UTC 2024

--- .cron/git-sync.sh
+++ .cron/git-sync.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Change directory to the repo directory
 repo=$(echo "$GITHUB_REPOSITORY" | cut -d'/' -f2)
-
 cd "/workspaces/$repo" || exit
+
+# Pull to avoid conflicts, then stage all changes.
 git pull
 git add .
 
-# Check if there are changes staged
+# Check if there are changes staged and sync.
 if ! git diff --cached --exit-code; then
     git commit -m "Automatic commit $(date)"
     git push
